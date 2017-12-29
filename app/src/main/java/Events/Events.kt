e0b9c8cf605bf796ea578cf4525608733f178606
Gary Jacobs <garyjacobs@Garys-MBP.hsd1.nj.comcast.net>
2017-12-29 12:49:41 -0500

    Add fling to combo box. Clean up combo box.

--- app/src/main/java/Events/Events.kt
+++ app/src/main/java/Events/Events.kt
@@ -4,5 +4,5 @@ package Events
  * Created by garyjacobs on 12/23/17.
  */
 class CurrentWeatherSelectedEvent()
-
+class MapClickedEvent()
 data class ForecastListSelectedEvent(val itemSelected: Int)
